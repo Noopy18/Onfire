0d81404c2321d6f445dbf322eb083c6e7f2ed254
StarOnimus <110303359+StarOnimus@users.noreply.github.com>
2025-11-18 16:44:05 +0000

    Finalização da parte principal do RBAC.

--- console/migrations/m251118_144537_rbac.php
+++ console/migrations/m251118_144537_rbac.php
@@ -48,6 +48,18 @@ class m251118_144537_rbac extends Migration
         $auth->add($deleteUser);
         $auth->addChild($technician, $deleteUser);
 
+        // ################################################## UTILIZADOR
+
+        //Permissão para a atualização de extras de um Utilizador.
+        $updateUtilizador = $auth->createPermission('updateUtilizador');
+        $auth->add($updateUtilizador);
+        $auth->addChild($technician, $updateUtilizador);
+
+        //Permissão para a vizualização de extras de um Utilizador.
+        $viewUtilizador = $auth->createPermission('viewUtilizador');
+        $auth->add($viewUtilizador);
+        $auth->addChild($technician, $viewUtilizador);
+
         // ################################################## CATEGORIA
 
         //Permissão para a criação de Categorias.
@@ -70,7 +82,7 @@ class m251118_144537_rbac extends Migration
         $auth->add($deleteCategory);
         $auth->addChild($technician, $deleteCategory);
 
-        // ################################################## Conquistas
+        // ################################################## CONQUISTAS
 
         //Permissão para a criação de Conquistas.
         $createBadge = $auth->createPermission('createBadge');
@@ -92,6 +104,66 @@ class m251118_144537_rbac extends Migration
         $auth->add($deleteBadge);
         $auth->addChild($technician, $deleteBadge);
 
+        // ################################################## HÁBITOS
+
+        //Permissão para a criação de Hábitos.
+        $createHabit = $auth->createPermission('createHabit');
+        $auth->add($createHabit);
+        $auth->addChild($user, $createHabit);
+
+        //Permissão para a atualização de Hábitos.
+        $updateHabit = $auth->createPermission('updateHabit');
+        $auth->add($updateHabit);
+        $auth->addChild($user, $updateHabit);
+
+        //Permissão para a vizualização de Hábitos.
+        $viewHabit = $auth->createPermission('viewHabit');
+        $auth->add($viewHabit);
+        $auth->addChild($user, $viewHabit);
+
+        //Permissão para a eliminação de Hábitos.
+        $deleteHabit = $auth->createPermission('deleteHabit');
+        $auth->add($deleteHabit);
+        $auth->addChild($user, $deleteHabit);
+
+        // ################################################## DESAFIOS SEMANAIS
+
+        //Permissão para a criação de Desafios Semanais.
+        $createWeeklychallenge = $auth->createPermission('createWeeklychallenge');
+        $auth->add($createWeeklychallenge);
+        $auth->addChild($technician, $createWeeklychallenge);
+
+        //Permissão para a atualização de Desafios Semanais.
+        $updateWeeklychallenge = $auth->createPermission('updateWeeklychallenge');
+        $auth->add($updateWeeklychallenge);
+        $auth->addChild($technician, $updateWeeklychallenge);
+
+        //Permissão para a vizualização de Desafios Semanais.
+        $viewWeeklychallenge = $auth->createPermission('viewWeeklychallenge');
+        $auth->add($viewWeeklychallenge);
+        $auth->addChild($user, $viewWeeklychallenge);
+
+        //Permissão para a eliminação de Desafios Semanais.
+        $deleteWeeklychallenge = $auth->createPermission('deleteWeeklychallenge');
+        $auth->add($deleteWeeklychallenge);
+        $auth->addChild($technician, $deleteWeeklychallenge);
+
+        // ################################################## AMIGOS
+
+        //Permissão para enviar um pedido de amizade.
+        $sendInvitation = $auth->createPermission('sendInvitation');
+        $auth->add($sendInvitation);
+        $auth->addChild($user, $sendInvitation);
+
+        //Permissão para rejeitar um pedido de amizade.
+        $rejectInvitation = $auth->createPermission('rejectInvitation');
+        $auth->add($rejectInvitation);
+        $auth->addChild($user, $rejectInvitation);
+
+        //Permissão para aceitar um pedido de amizade.
+        $acceptInvitation = $auth->createPermission('acceptInvitation');
+        $auth->add($acceptInvitation);
+        $auth->addChild($user, $acceptInvitation);
 
     
     }
