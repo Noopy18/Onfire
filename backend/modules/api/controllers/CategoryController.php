eebb5655f6256f398bd756b6b005a1075b4ecc6c
StarOnimus <110303359+StarOnimus@users.noreply.github.com>
2025-12-16 02:01:11 +0000

    API - Categories.

---
+++ backend/modules/api/controllers/CategoryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace backend\modules\api\controllers;
+
+use yii\rest\ActiveController;
+
+class CategoryController extends ActiveController
+{
+    public $modelClass = 'common\models\Category';
+}
+
