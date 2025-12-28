35b0779f9730f6c0179c0fd42b159ee53e22863a
StarOnimus <110303359+StarOnimus@users.noreply.github.com>
2025-12-28 11:46:21 +0000

    Minor fix.

--- backend/controllers/BadgeController.php
+++ backend/controllers/BadgeController.php
@@ -4,6 +4,7 @@ namespace backend\controllers;
 
 use common\models\Badge;
 use yii\data\ActiveDataProvider;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
