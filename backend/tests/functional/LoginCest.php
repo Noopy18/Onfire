4c6550180db343705bca08ce822c7fb2f1609d4e
StarOnimus <110303359+StarOnimus@users.noreply.github.com>
2026-01-03 21:28:36 +0000

    Codeception

--- backend/tests/functional/LoginCest.php
+++ backend/tests/functional/LoginCest.php
@@ -10,34 +10,14 @@ use common\fixtures\UserFixture;
  */
 class LoginCest
 {
-    /**
-     * Load fixtures before db transaction begin
-     * Called in _before()
-     * @see \Codeception\Module\Yii2::_before()
-     * @see \Codeception\Module\Yii2::loadFixtures()
-     * @return array
-     */
-    public function _fixtures()
-    {
-        return [
-            'user' => [
-                'class' => UserFixture::class,
-                'dataFile' => codecept_data_dir() . 'login_data.php'
-            ]
-        ];
-    }
-    
-    /**
-     * @param FunctionalTester $I
-     */
     public function loginUser(FunctionalTester $I)
     {
         $I->amOnRoute('/site/login');
-        $I->fillField('Username', 'erau');
-        $I->fillField('Password', 'password_0');
-        $I->click('login-button');
+        $I->fillField('Username', 'admin');
+        $I->fillField('Password', 'admin');
+        $I->click('Entrar');
 
-        $I->see('Logout (erau)', 'form button[type=submit]');
+        $I->see('[administrator] - admin', 'a[href="#"]');
         $I->dontSeeLink('Login');
         $I->dontSeeLink('Signup');
     }
